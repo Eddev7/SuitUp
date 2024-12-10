64f5c93f8531acf03df1c827225cf86b43c06e9d
Eddev7 <eduardo.nog7@gmail.com>
2024-12-10 13:05:00 -0300

    dist atualizado

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Configuração básica do Axios
 const api = axios.create({
-  baseURL: 'http://18.117.187.82:3000',
+  baseURL: 'http://127.0.0.1:3000',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
